40adefdc13f333ce70f2a130f90b2b0bec95e71c
Tamer Kilinc <info@webdesignpresse.com>
2020-09-02 16:32:18 +0200

    updated footer

--- footer-widget.php
+++ footer-widget.php
@@ -3,15 +3,21 @@
 if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) ) {?>
         <div id="footer-widget" class="row m-0">
 
-                    <?php if ( is_active_sidebar( 'footer-1' )) : ?>
-                        <div class="col-12 col-md-4 p-3 p-md-5"><?php dynamic_sidebar( 'footer-1' ); ?></div>
-                    <?php endif; ?>
-                    <?php if ( is_active_sidebar( 'footer-2' )) : ?>
-                        <div class="col-12 col-md-4 p-3 p-md-5"><?php dynamic_sidebar( 'footer-2' ); ?></div>
-                    <?php endif; ?>
-                    <?php if ( is_active_sidebar( 'footer-3' )) : ?>
-                        <div class="col-12 col-md-4 p-3 p-md-5"><?php dynamic_sidebar( 'footer-3' ); ?></div>
-                    <?php endif; ?>
+            <?php if ( is_active_sidebar( 'footer-1' )) : ?>
+                <div class="col-12 col-lg-4 p-3 p-md-5"><?php dynamic_sidebar( 'footer-1' ); ?></div>
+            <?php endif; ?>
+            <?php if ( is_active_sidebar( 'footer-2' )) : ?>
+                <div class="col-6 col-lg-2 p-3 p-md-5"><?php dynamic_sidebar( 'footer-2' ); ?></div>
+            <?php endif; ?>
+            <?php if ( is_active_sidebar( 'footer-3' )) : ?>
+                <div class="col-6 col-lg-2 p-3 p-md-5"><?php dynamic_sidebar( 'footer-3' ); ?></div>
+            <?php endif; ?>
+            <?php if ( is_active_sidebar( 'footer-4' )) : ?>
+                <div class="col-6 col-lg-2 p-3 p-md-5"><?php dynamic_sidebar( 'footer-4' ); ?></div>
+            <?php endif; ?>
+            <?php if ( is_active_sidebar( 'footer-5' )) : ?>
+                <div class="col-6 col-lg-2 p-3 p-md-5"><?php dynamic_sidebar( 'footer-5' ); ?></div>
+            <?php endif; ?>
 
         </div>
 

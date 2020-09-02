7445dafdbd047c0c7c1fbc9eb6efa33035c91131
Tamer Kilinc <info@webdesignpresse.com>
2020-09-02 16:17:19 +0200

    footer & header customizations

--- footer-one.php
+++ footer-one.php
@@ -21,18 +21,6 @@
 						&copy; <?php echo date('Y'); ?> <?php echo '<a href="'.home_url().'">'.get_bloginfo('name').'</a>'; ?>
 						<span>&nbsp;|&nbsp;<?php echo __('Alle Rechte vorbehalten.','wp-bootstrap-starter'); ?></span>
 					</div><!-- close .site-info -->
-					<div class="d-block d-md-inline text-center text-md-left">
-					<?php $website_type = get_theme_mod( 'select_website_type', 'one' );
-					if( $website_type == 'two' ) : ?>
-						<!-- single page -->
-						<a class="wdp_modal_link" data-toggle="modal" data-target="#impressumModal"><?php echo __('Impressum.','wp-bootstrap-starter'); ?></a>
-						<a class="wdp_modal_link" data-toggle="modal" data-target="#datenschutzModal"><?php echo __('Datenschutz.','wp-bootstrap-starter'); ?></a>
-					<?php else : ?>
-						<!-- multi page -->
-						<a href="/impressum"><?php echo __('Impressum.','wp-bootstrap-starter'); ?></a>
-						<a href="/datenschutz"><?php echo __('Datenschutz.','wp-bootstrap-starter'); ?></a>
-					<?php endif; ?>
-					</div>
 				</div>
 				<div class="col-md-2 col-12 text-center text-md-right order-1 order-md-2">
 					<a id="scrollToTop" href="#page" class="page-scroll-tk"><i class="fas fa-chevron-up fa-2x"></i></a>

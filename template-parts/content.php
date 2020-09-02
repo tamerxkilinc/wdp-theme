95ead1a114ed677cec0634da08ac2f19fb06cd07
Tamer Kilinc <info@webdesignpresse.com>
2020-09-02 17:40:56 +0200

    Update content.php

--- template-parts/content.php
+++ template-parts/content.php
@@ -14,9 +14,9 @@
 	</div>
 	<header class="entry-header">
 		<?php
-		if ( is_single() ) :
+		if ( is_single() && !is_home() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
+		elseif ( !is_home() ) :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 

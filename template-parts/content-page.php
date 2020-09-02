01f1ab6d41d3c89acba4a23fac64bdbb57996654
Tamer Kilinc <info@webdesignpresse.com>
2020-09-02 17:37:15 +0200

    removed entry-header for homepage

--- template-parts/content-page.php
+++ template-parts/content-page.php
@@ -11,9 +11,11 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
-    <header class="entry-header">
+	<?php if ( !is_home() ) : ?>
+	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 	</header><!-- .entry-header -->
+	<?php endif; ?>
 
 	<div class="entry-content">
 		<?php

34665c500f30883422851dd9253a97c424c3acb9
oezbek-wdp <73443133+oezbek-wdp@users.noreply.github.com>
2020-11-06 13:09:44 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,7 +17,7 @@ if ( $website_type == 'two' ) :
 			<div class="modal-dialog" role="document">
 				<div class="modal-content">
 					<div class="modal-header">
-						<h3 class="modal-title" id="datenschutzModal">Datenschutz</h3>
+						<h3 class="modal-title" id="datenschutzModalTitle">Datenschutz</h3>
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 							<span aria-hidden="true">&times;</span>
 						</button>

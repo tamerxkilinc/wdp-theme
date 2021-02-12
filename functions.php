6d1e431c76796ab30152bfc794e49754e294f52f
oezbek-wdp <73443133+oezbek-wdp@users.noreply.github.com>
2021-02-12 16:46:46 +0100

    Footer changes

--- functions.php
+++ functions.php
@@ -139,6 +139,15 @@ function wp_bootstrap_starter_widgets_init() {
         'after_title'   => '</h3>',
 	) );
 	register_sidebar( array(
+        'name'          => esc_html__( 'Footer 4', 'wp-bootstrap-starter' ),
+        'id'            => 'footer-4',
+        'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h3 class="widget-title">',
+        'after_title'   => '</h3>',
+	) );
+	register_sidebar( array(
         'name'          => esc_html__( 'Topbar Left 1', 'wp-bootstrap-starter' ),
         'id'            => 'topbar-l1',
         'description'   => esc_html__( 'Add widgets here.', 'wp-bootstrap-starter' ),

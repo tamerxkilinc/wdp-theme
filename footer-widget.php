8d06941c72cceb40dbabe7462d2c1953d5894407
oezbek-wdp <73443133+oezbek-wdp@users.noreply.github.com>
2021-03-25 15:08:47 +0100

    Update footer-widget.php

--- footer-widget.php
+++ footer-widget.php
@@ -1,3 +1,27 @@
+<!-- Payments section -->
+					<section id="payments-section" class="bg-white">
+					<div class="container">
+						<div class="row py-5 text-center">
+							<div class="col-12">
+								<h4 class="mt-0 mb-5">Zahlungsmethoden</h4>
+								<div class="wdp-row justify-content-center">
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/klarna2.png" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/klarna3.png" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/Klarna-1.png" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/logo_electroniccash.jpg" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/pp.png" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/americanex.jpg" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/mastercard.png" alt=""></div>
+									<div><img class="footer-icon" src="<?=get_template_directory_uri();?>/inc/assets/images/visa.png" alt=""></div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+					</section>
+					<!-- end payments -->
+
+
 <?php
 
 if ( is_active_sidebar( 'footer-1' ) || is_active_sidebar( 'footer-2' ) || is_active_sidebar( 'footer-3' ) || is_active_sidebar( 'footer-4' ) ) {?>
